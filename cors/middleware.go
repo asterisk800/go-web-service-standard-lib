e35d5a37f478f77d01a61d293e9d4db60be78752
Gudina H <asterisk800@users.noreply.github.com>
2020-12-11 15:10:47 -0800

    Organized the code into packages and added CORS

---
+++ cors/middleware.go
@@ -0,0 +1,14 @@
+package cors
+
+import "net/http"
+
+// Middleware :
+func Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept")
+		handler.ServeHTTP(w, r)
+	})
+}
---
+++ product/product.go
@@ -0,0 +1,12 @@
+package product
+
+// Product :
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
+}
